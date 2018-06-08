8b434cdc4b2ad0921a034fa6ffa60232c2054640
Zalron <33308793+Zalron@users.noreply.github.com>
2018-06-08 19:04:25 +1000

    Updated Project

---
+++ Exclusion(Project Files)/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour 
+{
+	public UIBehaviour UIB;
+	void Start () // Use this for initialization
+	{
+		
+	}
+	void Update () // Update is called once per frame
+	{
+		
+	}
+	void OnTriggerEnter (Collider other) // function OnTriggerEnter, for if the player enters the collider of the key and it switchs the object off and turns on the variable hasKey
+	{
+		if (other.CompareTag("Key")) // compares the tag that the object is named key
+		{
+			UIB.hasKey = true;
+			other.gameObject.SetActive(false);
+		}
+	}
+}
