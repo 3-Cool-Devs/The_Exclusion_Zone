b115966a4bab061e90d2578955c13958c4f946da
Zalron <33308793+Zalron@users.noreply.github.com>
2018-06-15 13:25:25 +1000

    Logic Rewrite (pt2)

---
+++ Exclusion(Project Files)/Assets/Scripts/Test(not used)/TestBehaviourBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviourBase : StateMachineBehaviour
+{
+    public GameObject Demon;
+    public GameObject Player;
+    public Animator anim;
+    public float waypointRange = 3.0f;
+    public float waypointRotationSpeed = 1.0f;
+    public float waypointSpeed = 2.0f;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Demon = animator.gameObject;
+        Player = Demon.GetComponent<DemonAI>().GetPlayer();
+        currentWP = 0;
+    }
+    void Start () // Use this for initialization
+    {
+		
+	}
+	void Update () // Update is called once per frame
+    {
+		
+	}
+}
--- Exclusion(Project Files)/Assets/Scripts/Test(not used)/TestBehaviourFSMPatrol.cs
+++ Exclusion(Project Files)/Assets/Scripts/Test(not used)/TestBehaviourFSMPatrol.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 using UnityEngine;
 
 public class TestBehaviourFSMPatrol : StateMachineBehaviour
 {
     //private NavMeshAgent myAgent;
-    public Transform target;
+    
     //
-    GameObject NPC;
+   
     public GameObject[] waypoints;
-    public float waypointRange = 3.0f;
-    public float waypointRotationSpeed = 1.0f;
-    public float waypointSpeed = 2.0f;
+    
     public int currentWP;
+    private float angle;
+    public float demonRotation = 0.1f;
+    public float demonNoticeRange = 10f;
+    public float demonChaseRange = 5f;
+    public float demonNoticeFOV;
+    public float demonSpeed = 1.5f;
+    public float demonChaseSpeed = 2f;
+    public float demonDamage = 5f;
+    private float angleBetweenDemonAndPlayer;
+    public UIBehaviour uiBehav;
+    public LayerMask viewMask;
     private void Awake()
     {
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
@@ -30,6 +40,7 @@ public class TestBehaviourFSMPatrol : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
     {
         NPC = animator.gameObject;
+        anim = GetComponent<Animator>();
         currentWP = 0;
     }
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -52,6 +63,16 @@ public class TestBehaviourFSMPatrol : StateMachineBehaviour
         direction.y = 0;
         NPC.transform.rotation = Quaternion.Slerp (NPC.transform.rotation, Quaternion.LookRotation (direction), waypointRotationSpeed * Time.deltaTime);
         NPC.transform.Translate (0, 0, Time.deltaTime * waypointSpeed);
+        if (Vector3.Distance(target.position, NPC.transform.position) < demonNoticeRange && angle < demonNoticeFOV && !Physics.Linecast(NPC.transform.position, target.position, viewMask)) //|| isChasing == true)
+        {
+            uiBehav.hasBeenSpotted = true;
+            NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, Quaternion.LookRotation(direction), demonRotation * Time.deltaTime);
+            if (direction.magnitude < demonChaseRange) // if you enter it is chase range it chases you
+            {
+                anim.doesSee = true;
+                NPC.transform.Translate(0, 0, demonChaseSpeed * Time.deltaTime);
+            }
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
